2fb51ab878d2e27954b4dd2c3a5b011abba07dd0
nerotvlive <daniel.niesmann@outlook.com>
2026-06-21 19:05:59 +0200

    Rename all "downloads" references to "settings" in CSS, JavaScript, and HTML, and update button icon for new settings usage.

--- src/main/html/assets/nexus/js/settings.js
+++ src/main/html/assets/nexus/js/settings.js
@@ -1,129 +1,129 @@
-function setRunningDownload(id,name,timeElapsed,timeRemaining,sizeDownload,sizeFile,progress,speed,url,path) {
-    if(document.getElementById("running-download")) {
-        document.getElementById("running-download").remove();
-        document.getElementById("downloads-loader-card").classList.remove("d-none");
+function setRunningSetting(id,name,timeElapsed,timeRemaining,sizeSetting,sizeFile,progress,speed,url,path) {
+    if(document.getElementById("running-setting")) {
+        document.getElementById("running-setting").remove();
+        document.getElementById("settings-loader-card").classList.remove("d-none");
     }
 
-    if(id && name && timeElapsed && timeRemaining && sizeDownload && sizeFile && progress && speed && url && path) {
+    if(id && name && timeElapsed && timeRemaining && sizeSetting && sizeFile && progress && speed && url && path) {
         if(document.getElementById(id)) {
             document.getElementById(id).remove();
         }
-        const template = document.getElementById("running-download-template");
-        const download = template.cloneNode(true);
-        download.id = "running-download";
-        download.classList.remove("d-none");
-        download.classList.add(id);
-        download.querySelector(".download-name").innerHTML = "<strong>"+name+"</strong>";
-        download.querySelector(".downloadId").innerText = id;
-        download.querySelector(".downloadUrl").innerText = url;
-        download.querySelector(".downloadPath").innerText = path;
-        download.querySelector(".timeElapsed").innerText = timeElapsed;
-        download.querySelector(".timeRemaining").innerText = timeRemaining;
-        download.querySelector(".downloadSize").innerText = sizeDownload;
-        download.querySelector(".fileSize").innerText = sizeFile;
-        download.querySelector(".progressInfo").innerText = progress+"%";
-        download.querySelector(".downloadSpeed").innerText = speed;
-        download.querySelector(".progress-bar").style.width = progress+"%";
+        const template = document.getElementById("running-setting-template");
+        const setting = template.cloneNode(true);
+        setting.id = "running-setting";
+        setting.classList.remove("d-none");
+        setting.classList.add(id);
+        setting.querySelector(".setting-name").innerHTML = "<strong>"+name+"</strong>";
+        setting.querySelector(".settingId").innerText = id;
+        setting.querySelector(".settingUrl").innerText = url;
+        setting.querySelector(".settingPath").innerText = path;
+        setting.querySelector(".timeElapsed").innerText = timeElapsed;
+        setting.querySelector(".timeRemaining").innerText = timeRemaining;
+        setting.querySelector(".settingSize").innerText = sizeSetting;
+        setting.querySelector(".fileSize").innerText = sizeFile;
+        setting.querySelector(".progressInfo").innerText = progress+"%";
+        setting.querySelector(".settingSpeed").innerText = speed;
+        setting.querySelector(".progress-bar").style.width = progress+"%";
 
         if(getStorageItem("devtools")) {
             if(getStorageItem("devtools") === "true") {
-                download.querySelector(".debug-info").classList.remove("d-none");
+                setting.querySelector(".debug-info").classList.remove("d-none");
             }
         }
 
-        template.parentNode.insertBefore(download, template);
-        document.getElementById("downloads-loader-card").classList.add("d-none");
+        template.parentNode.insertBefore(setting, template);
+        document.getElementById("settings-loader-card").classList.add("d-none");
     }
 }
 
-function updateRunningDownload(id,name,timeElapsed,timeRemaining,sizeDownload,sizeFile,progress,speed,url,path) {
-    if(id && name && timeElapsed && timeRemaining && sizeDownload && sizeFile && progress && speed && url && path) {
-        if (document.getElementById("running-download")) {
-            const download = document.getElementById("running-download");
-            if (download.classList.contains(id)) {
-                download.querySelector(".timeElapsed").innerText = timeElapsed;
-                download.querySelector(".timeRemaining").innerText = timeRemaining;
-                download.querySelector(".fileSize").innerText = sizeFile;
-                download.querySelector(".progressInfo").innerText = progress+"%";
-                download.querySelector(".progress-bar").style.width = progress+"%";
-                download.querySelector(".downloadSpeed").innerText = speed;
+function updateRunningSetting(id,name,timeElapsed,timeRemaining,sizeSetting,sizeFile,progress,speed,url,path) {
+    if(id && name && timeElapsed && timeRemaining && sizeSetting && sizeFile && progress && speed && url && path) {
+        if (document.getElementById("running-setting")) {
+            const setting = document.getElementById("running-setting");
+            if (setting.classList.contains(id)) {
+                setting.querySelector(".timeElapsed").innerText = timeElapsed;
+                setting.querySelector(".timeRemaining").innerText = timeRemaining;
+                setting.querySelector(".fileSize").innerText = sizeFile;
+                setting.querySelector(".progressInfo").innerText = progress+"%";
+                setting.querySelector(".progress-bar").style.width = progress+"%";
+                setting.querySelector(".settingSpeed").innerText = speed;
 
                 if(getStorageItem("devtools")) {
                     if(getStorageItem("devtools") === "true") {
-                        download.querySelector(".debug-info").classList.remove("d-none");
+                        setting.querySelector(".debug-info").classList.remove("d-none");
                     }
                 }
                 return;
             }
         }
-        setRunningDownload(id, name, timeElapsed, timeRemaining, sizeDownload, sizeFile, progress, speed, url, path);
+        setRunningSetting(id, name, timeElapsed, timeRemaining, sizeSetting, sizeFile, progress, speed, url, path);
     }
 }
 
-function addHistoryDownload(id,name,success) {
+function addHistorySetting(id,name,success) {
     if(id && name) {
-        if(document.getElementById("running-download")) {
-            if(document.getElementById("running-download").classList.contains(id.replace("-history", ""))) {
-                document.getElementById("running-download").remove();
-                document.getElementById("downloads-loader-card").classList.remove("d-none");
+        if(document.getElementById("running-setting")) {
+            if(document.getElementById("running-setting").classList.contains(id.replace("-history", ""))) {
+                document.getElementById("running-setting").remove();
+                document.getElementById("settings-loader-card").classList.remove("d-none");
             }
         }
         if (!document.getElementById(id)) {
-            const template = document.getElementById("history-download-template");
-            const download = template.cloneNode(true);
-            download.id = id;
-            download.classList.remove("d-none");
-            download.querySelector(".downloadName").innerText = name;
+            const template = document.getElementById("history-setting-template");
+            const setting = template.cloneNode(true);
+            setting.id = id;
+            setting.classList.remove("d-none");
+            setting.querySelector(".settingName").innerText = name;
             if (success === true) {
-                download.classList.add("finished");
-                download.querySelector(".downloadSuccessIcon").classList.add("bi-check-lg");
+                setting.classList.add("finished");
+                setting.querySelector(".settingSuccessIcon").classList.add("bi-check-lg");
             } else if (success === false) {
-                download.classList.add("failed");
-                download.querySelector(".downloadSuccessIcon").classList.add("bi-x-lg");
+                setting.classList.add("failed");
+                setting.querySelector(".settingSuccessIcon").classList.add("bi-x-lg");
             }
-            template.parentNode.insertBefore(download, template);
+            template.parentNode.insertBefore(setting, template);
         }
     }
 }
 
-function addWaitingDownload(id,name,url,path,index) {
+function addWaitingSetting(id,name,url,path,index) {
     if(id && name && url && path && index) {
         if (!document.getElementById(id)) {
-            if (document.getElementById("running-download")) {
-                if (document.getElementById("running-download").classList.contains(id)) {
-                    document.getElementById("running-download").remove();
-                    document.getElementById("downloads-loader-card").classList.remove("d-none");
+            if (document.getElementById("running-setting")) {
+                if (document.getElementById("running-setting").classList.contains(id)) {
+                    document.getElementById("running-setting").remove();
+                    document.getElementById("settings-loader-card").classList.remove("d-none");
                 }
             }
-            const template = document.getElementById("waiting-download-template");
-            const download = template.cloneNode(true);
-            download.id = id;
-            download.classList.remove("d-none");
-            download.querySelector(".download-name").innerText = name;
-            download.querySelector(".downloadId").innerText = id;
-            download.querySelector(".downloadUrl").innerText = url;
-            download.querySelector(".downloadPath").innerText = path;
-            download.querySelector(".downloadIndex").innerText = index;
+            const template = document.getElementById("waiting-setting-template");
+            const setting = template.cloneNode(true);
+            setting.id = id;
+            setting.classList.remove("d-none");
+            setting.querySelector(".setting-name").innerText = name;
+            setting.querySelector(".settingId").innerText = id;
+            setting.querySelector(".settingUrl").innerText = url;
+            setting.querySelector(".settingPath").innerText = path;
+            setting.querySelector(".settingIndex").innerText = index;
             if (getStorageItem("devtools")) {
                 if (getStorageItem("devtools") === "true") {
-                    download.querySelector(".debug-info").classList.remove("d-none");
+                    setting.querySelector(".debug-info").classList.remove("d-none");
                 }
             }
-            template.parentNode.insertBefore(download, template);
+            template.parentNode.insertBefore(setting, template);
         }
     }
 }
 
-function initDownloads() {
-    console.log("[CONNECTOR] downloads.init");
+function initSettings() {
+    console.log("[CONNECTOR] settings.init");
     initLoader();
 }
-initDownloads();
+initSettings();
 
 async function initLoader() {
     setTimeout(function (){
-        if(document.getElementById("downloads-loader")) {
-            document.getElementById("downloads-loader").innerHTML = "<strong>No running download!</strong>";
+        if(document.getElementById("settings-loader")) {
+            document.getElementById("settings-loader").innerHTML = "<strong>Not implemented!</strong>";
         }
     }, 2000);
 }
