ed6b20fa551fdeb0ec70d4f5e7c48e988f039c4d
nerotvlive <daniel.niesmann@outlook.com>
2026-09-02 01:19:57 +0200

    Add toggle functionality to Library Menu, update MenuView props and styles for improved interactivity and animation

--- src/frontend/src/components/MenuView.vue
+++ src/frontend/src/components/MenuView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import '@/assets/zyneonstudios/styles/components/menuview.css';
+
+defineProps<{
+  isDisabled?: boolean;
+}>();
 </script>
 
 <template>
   <div class="flex menuview">
-    <div class="flex flex-col p-3 menu">
+    <div class="flex flex-col p-3 menu" :class="{ disabled: isDisabled }">
       <slot name="menu"></slot>
     </div>
     <div class="content grow">
--- src/frontend/src/pages/Library.vue
+++ src/frontend/src/pages/Library.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
 import MenuView from "@/components/MenuView.vue";
+
+const isMenuDisabled = ref(false);
+
+const toggleMenu = () => {
+  isMenuDisabled.value = !isMenuDisabled.value;
+};
 </script>
 
 <template>
   <div class="library h-full w-full">
-    <MenuView>
+    <MenuView :isDisabled="isMenuDisabled">
       <template #menu>
-        <button>
-          <i class="bi bi-grid-3x3-gap-fill"></i>
-          Overview
-        </button>
+        <div class="flex gap-1">
+          <button @click="toggleMenu" :class="{ 'rotate-0': !isMenuDisabled, 'rotate-180': isMenuDisabled }">
+            <i class="bi bi-arrow-bar-left"></i>
+          </button>
+          <button class="grow">
+            <i class="bi bi-grid-3x3-gap-fill"></i>
+            Overview
+          </button>
+        </div>
         <div class="flex gap-1">
           <button class="grow">
             <i class="bi bi-plus-lg"></i>
@@ -23,7 +34,9 @@ import MenuView from "@/components/MenuView.vue";
         <hr>
       </template>
       <template #content>
-
+        <button @click="toggleMenu" class="absolute left-3 top-3 bg-zinc-800 h-8 w-8 rounded-lg shadow-lg shadow-black/25 hover:bg-zinc-700 hover:cursor-pointer transition-all" :class="{ 'hide rotate-180': !isMenuDisabled, 'rotate-0': isMenuDisabled }">
+          <i class="bi bi-arrow-bar-right transition-all"></i>
+        </button>
       </template>
     </MenuView>
   </div>
@@ -31,6 +44,9 @@ import MenuView from "@/components/MenuView.vue";
 
 <style scoped>
 .library {
-
+  .hide {
+    opacity: 0;
+    z-index: -1;
+  }
 }
 </style>
