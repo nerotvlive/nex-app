9900e69ec119569a6213a03db99f51a6b41f4dc6
Blockomega <christian2008@gmx.de>
2026-09-12 19:24:14 +0200

    add first version of modrinth search integration

--- src/frontend/src/components/DashboardSearch.vue
+++ src/frontend/src/components/DashboardSearch.vue
@@ -4,6 +4,7 @@ import { ref, onMounted } from "vue";
 import type { InstanceItem } from "@/data/mockInstances";
 import MenuView from "@/components/MenuView.vue";
 import { useRoute } from "vue-router";
+import {SearchResponse, SearchResultItem} from "@/types/modrinth";
 
 const props = defineProps<{
   instances: InstanceItem[];
@@ -14,14 +15,48 @@ const searchBar = ref<HTMLInputElement | null>(null);
 const route = useRoute();
 
 const emit = defineEmits<{
-  (e: 'select', instance: InstanceItem): void;
-  (e: 'install', instance: InstanceItem): void;
+  (e: 'select', project: SearchResultItem): void;
+  (e: 'install', project: SearchResultItem): void;
 }>();
 
 const viewMode = ref<'grid' | 'list'>('grid');
 const searchQuery = ref('');
 
-console.log(route.query.q);
+
+
+const results = ref<SearchResultItem[]>([])
+const loading = ref<boolean>(false);
+const error = ref<string | null>(null);
+
+async function searchModrinth() {
+  const query = searchQuery.value.trim();
+  if (!query) {
+    results.value = [];
+    return;
+  }
+
+  loading.value = true;
+  error.value = null;
+
+  try {
+    const response = await fetch(`https://api.modrinth.com/v2/search?${new URLSearchParams({
+      query,
+      limit: '24',
+      index: 'relevance'
+    })}`);
+    if (!response.ok) {
+      window.alert("Modrinth Wallah Krise: " + response.statusText);
+    }
+
+    const data: SearchResponse = await response.json();
+    results.value = data.hits;
+
+  } catch (e) {
+    window.alert("Wallah Krise: " + e);
+  } finally {
+    loading.value = false;
+  }
+}
 
 onMounted(async () => {
   searchBar.value?.focus();
@@ -54,27 +89,32 @@ onMounted(async () => {
             </div>
           </template>
           <template #menu>
-            <input v-model="searchQuery" ref="searchBar" type="text" placeholder="Search resources..." class="h-fit w-fit py-2 px-4 text-sm bg-zinc-500/25 hover:bg-zinc-400/25 text-white rounded transition hover:shadow-md focus:shadow-md shadow-black/25" />
+            <input v-model="searchQuery" ref="searchBar" type="text" placeholder="Search resources..." @keyup.enter="searchModrinth()" class="h-fit w-fit py-2 px-4 text-sm bg-zinc-500/25 hover:bg-zinc-400/25 text-white rounded transition hover:shadow-md focus:shadow-md shadow-black/25" />
           </template>
         </MenuBar>
         <div class="grow overflow-y-auto overflow-hidden overview-bg p-3 pr-1">
           <div v-if="viewMode === 'grid'" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 pr-2 pb-6">
-            <div v-for="inst in instances" :key="inst.id" @click="emit('select', inst)" class="bg-zinc-600/25 hover:bg-zinc-500/25 border border-zinc-700 hover:border-zinc-600 rounded-lg p-4 flex flex-col justify-between transition cursor-pointer group shadow-lg">
+            <div v-for="proj in results" :key="proj.project_id" @click="emit('select', proj)" class="bg-zinc-600/25 hover:bg-zinc-500/25 border border-zinc-700 hover:border-zinc-600 rounded-lg p-4 flex flex-col justify-between transition cursor-pointer group shadow-lg">
               <div>
                 <div class="flex items-start justify-between mb-3">
                   <div class="w-12 h-12 rounded-md bg-zinc-700 flex items-center justify-center text-xl text-white group-hover:scale-105 transition">
-                    <i :class="['bi', inst.icon]"></i>
+                    <img
+                        v-if="proj.icon_url"
+                        :src="proj.icon_url"
+                        :alt="proj.title"
+                        class="w-full h-full object-cover rounded-md"
+                    />
                   </div>
                   <span class="text-xs px-2.5 py-1 rounded-lg bg-zinc-800 text-zinc-300">
-                {{ inst.loader }} {{ inst.version }}
+                {{ proj.project_type }} - {{ proj.versions.slice(0, 2).join(', ') }}
               </span>
                 </div>
-                <h3 class="font-bold text-lg mb-1">{{ inst.title }}</h3>
-                <p class="text-sm text-zinc-300">{{ inst.description }}</p>
+                <h3 class="font-bold text-lg mb-1">{{ proj.title }}</h3>
+                <p class="text-sm text-zinc-300">{{ proj.description }}</p>
               </div>
 
               <div class="flex justify-end gap-2 mt-4 pt-3 border-t border-t-zinc-700">
-                <button @click.stop="emit('install', inst)" class="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1.5 rounded-lg text-sm font-bold flex items-center gap-2 transition cursor-pointer shadow-md">
+                <button @click.stop="emit('install', proj)" class="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1.5 rounded-lg text-sm font-bold flex items-center gap-2 transition cursor-pointer shadow-md">
                   <i class="bi bi-download"></i> Install
                 </button>
               </div>
@@ -83,23 +123,28 @@ onMounted(async () => {
 
 
           <div v-else class="flex flex-col gap-2 pr-2 pb-6">
-            <div v-for="inst in instances" :key="inst.id" @click="emit('select', inst)" class="bg-zinc-600/25 hover:bg-zinc-500/25 border border-zinc-700 hover:border-zinc-600 rounded-lg p-3 px-4 flex items-center justify-between transition cursor-pointer shadow-md">
+            <div v-for="proj in results" :key="proj.project_id" @click="emit('select', proj)" class="bg-zinc-600/25 hover:bg-zinc-500/25 border border-zinc-700 hover:border-zinc-600 rounded-lg p-3 px-4 flex items-center justify-between transition cursor-pointer shadow-md">
               <div class="flex items-center gap-4">
                 <div class="w-10 h-10 rounded-lg bg-zinc-700 flex items-center justify-center text-lg text-white-400">
-                  <i :class="['bi', inst.icon]"></i>
+                  <img
+                      v-if="proj.icon_url"
+                      :src="proj.icon_url"
+                      :alt="proj.title"
+                      class="w-full h-full object-cover rounded-md"
+                  />
                 </div>
                 <div>
                   <div class="flex gap-2">
-                    <h3 class="font-bold text-base">{{ inst.title }}</h3>
+                    <h3 class="font-bold text-base">{{ proj.title }}</h3>
                     <span class="text-xs px-2.5 py-1 rounded-lg bg-zinc-800 text-zinc-300">
-                  {{ inst.loader }} {{ inst.version }}
+                  {{ proj.project_type }} - {{ proj.versions.slice(0, 2).join(', ') }}
                 </span>
                   </div>
-                  <p class="text-sm text-zinc-300">{{ inst.description }}</p>
+                  <p class="text-sm text-zinc-300">{{ proj.description }}</p>
                 </div>
               </div>
               <div class="flex items-center gap-2">
-                <button @click.stop="emit('install', inst)" class="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1.5 rounded-lg text-sm font-bold flex items-center gap-2 transition cursor-pointer shadow-md">
+                <button @click.stop="emit('install', proj)" class="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1.5 rounded-lg text-sm font-bold flex items-center gap-2 transition cursor-pointer shadow-md">
                   <i class="bi bi-download"></i> Install
                 </button>
               </div>
